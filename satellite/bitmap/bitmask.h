7482aa55b4d39287c95d289f7b2d2bcec5b02772
Phil Austin <paustin@eos.ubc.ca>
2013-02-02 10:47:01 -0800

    forgot the header

---
+++ satellite/bitmap/bitmask.h
@@ -0,0 +1,5 @@
+void readcloud_cpp(signed char* byteone,signed char* maskout,int nvals);
+void readland_cpp(signed char* byteone,signed char* landout,int nvals);
+void readthin_cirrus_cpp(signed char* byteone,signed char* cirrusout,int nvals);
+void readhigh_cloud_cpp(signed char* byteone,signed char* highout,int nvals);
+
